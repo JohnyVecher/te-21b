5a771fc0304affaface4eb710d75addf814a7680
JohnyVecher <terra.stepanova@yandex.ru>
2024-09-20 16:22:04 +0500

    news

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,15 +4,15 @@ import './Portfolio.css';
 const PortfoliosList = () => {
   const portfolios = [
     { discipline: 'Многоканальные телекоммуникационные системы', name: 'MЭС', email: 'https://docs.google.com/spreadsheets/d/1bQIJequKaj0Od3ELZpSzbOoRD2_HZKoB9izY1NQTm4U/edit#gid=28697151' },
-    { discipline: 'Физические основы радиосвязи', name: 'хз', email: '' },
+    { discipline: 'Физические основы радиосвязи', name: '', email: '' },
     { discipline: 'Сети связи и системы коммутации', name: 'MЭС', email: 'https://docs.google.com/spreadsheets/d/1bQIJequKaj0Od3ELZpSzbOoRD2_HZKoB9izY1NQTm4U/edit#gid=28697151' },
     { discipline: 'Элективные дисциплины по физической культуре и спорту', name: 'на кафедре физры как не странно', email: '' },
     { discipline: 'Направляющие системы электросвязи', name: 'MЭС', email: 'https://docs.google.com/spreadsheets/d/1bQIJequKaj0Od3ELZpSzbOoRD2_HZKoB9izY1NQTm4U/edit#gid=28697151' },
-    { discipline: 'Обработка экспериментальных данных', name: 'хз', email: '' },
-    { discipline: 'Безопасность жизнедеятельности', name: 'хз', email: '' },
+    { discipline: 'Обработка экспериментальных данных', name: '', email: '' },
+    { discipline: 'Безопасность жизнедеятельности', name: '', email: '' },
     { discipline: 'Компьютерное моделирование', name: 'MЭС', email: 'https://docs.google.com/spreadsheets/d/1bQIJequKaj0Od3ELZpSzbOoRD2_HZKoB9izY1NQTm4U/edit#gid=28697151' },
     { discipline: 'Перспективные технологии в отрасли инфокоммуникаций', name: 'ИТИМС', email: 'https://docs.google.com/spreadsheets/d/19fM55yB0fwvguEwH60H3SMcrrPytq47SLuyob3qqVpg/edit#gid=1217056741' },
-    { discipline: 'Оптоэлектроника и нанофотоника', name: 'хз', email: '' },
+    { discipline: 'Оптоэлектроника и нанофотоника', name: '', email: '' },
   ];
 
   return (
