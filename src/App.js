b40ee72c61f4be12f25518c5f1538d24989a7737
JohnyVecher <terra.stepanova@yandex.ru>
2024-11-04 00:15:42 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { ThemeContext, ThemeProvider } from './components/ThemeContext'; 
+import { ThemeContext, ThemeProvider } from './components/ThemeContext';
 import TE21B from './components/TE21B';
 import TE31B from './components/TE31B';
 import Portfolio from './components/Portfolio';
@@ -11,49 +11,50 @@ import GroupSelectionTE2 from './components/GroupSelectionTE2';
 import './App.css';
 import logo from './logo.png';
 import night from './night.png';
-import soundFile from './sound.mp3'; // Убедитесь, что путь к звуку указан правильно
-import logoImage from './logo512.png'; // Импортируем изображение
+import soundFile from './sound.mp3';
+import logoImage from './logo512.png';
 
 const App = () => {
   const { isDarkMode, toggleTheme } = useContext(ThemeContext);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [isFullyOpened, setIsFullyOpened] = useState(false);
-  const [clickCount, setClickCount] = useState(0); // Состояние для отслеживания нажатий на кнопку
+  const [clickCount, setClickCount] = useState(0);
   const [isMessageVisible, setIsMessageVisible] = useState(false);
+  const [isMainContentVisible, setIsMainContentVisible] = useState(false);
 
   // Код для приветственного сообщения
   useEffect(() => {
-  setIsMessageVisible(true);
-  const timer = setTimeout(() => {
-    setIsMessageVisible(false);
-  }, 2000); // Убираем сообщение через 4 секунды (2 секунды вращение + 2 секунды исчезновение)
+    setIsMessageVisible(true);
+    const timer = setTimeout(() => {
+      setIsMessageVisible(false);
+      setIsMainContentVisible(true); // Показываем основной контент после приветствия
+    }, 2000);
 
-  return () => clearTimeout(timer); // Чистим таймер
-}, []);
+    return () => clearTimeout(timer);
+  }, []);
 
   const toggleDrawer = () => {
     if (isDrawerOpen) {
-      setIsFullyOpened(false); // Вернем полоски к исходному состоянию
-      setTimeout(() => setIsDrawerOpen(false), 300); // Закроем шторку после анимации
+      setIsFullyOpened(false);
+      setTimeout(() => setIsDrawerOpen(false), 300);
     } else {
-      setIsDrawerOpen(true); // Открытие шторки
-      setTimeout(() => setIsFullyOpened(true), 300); // Добавим крестик после анимации
+      setIsDrawerOpen(true);
+      setTimeout(() => setIsFullyOpened(true), 300);
     }
   };
 
   const handleLogoClick = () => {
-    const audio = new Audio(soundFile); // Создаем новый объект Audio
+    const audio = new Audio(soundFile);
 
     setClickCount((prevCount) => {
       const newCount = prevCount + 1;
 
-      // Проверяем, достигли ли мы 10 нажатий
       if (newCount === 10) {
-        audio.play(); // Проигрываем звук
-        return 0; // Сбрасываем счетчик
+        audio.play();
+        return 0;
       }
 
-      return newCount; // Возвращаем новое значение счетчика
+      return newCount;
     });
   };
 
@@ -63,62 +64,63 @@ const App = () => {
         {/* Приветственное сообщение с изображением */}
         {isMessageVisible && (
           <div className="welcome-message">
-		  <div class='hero'>
-  </div>
-  <div class='content'>
-    <h1 class='h1--scalingSize' data-text='An awesome title'>Добро пожаловать
-    </h1>
-  </div>
+            <div className="hero"></div>
+            <div className="content">
+              <h1 className="h1--scalingSize" data-text="An awesome title">Добро пожаловать</h1>
+            </div>
           </div>
         )}
 
-        {/* Верхняя панель с логотипом и кнопками */}
-        <div className="header">
-          <button className="logo-button" onClick={handleLogoClick}>
-            <img src={logo} alt="Logo" className="logo-image" />
-          </button>
-          <div className="header-buttons1">
-            <Link to="/" className="header-button">Расписание</Link>
-            <Link to="/portfolio" className="header-button">Портфолио</Link>
-            <Link to="/session" className="header-button">Сессия</Link>
-            <Link to="/info" className="header-button">Инфо</Link>
+        {/* Основной контент */}
+        <div className={`main-content ${isMainContentVisible ? 'visible' : ''}`}>
+          {/* Верхняя панель с логотипом и кнопками */}
+          <div className="header">
+            <button className="logo-button" onClick={handleLogoClick}>
+              <img src={logo} alt="Logo" className="logo-image" />
+            </button>
+            <div className="header-buttons1">
+              <Link to="/" className="header-button">Расписание</Link>
+              <Link to="/portfolio" className="header-button">Портфолио</Link>
+              <Link to="/session" className="header-button">Сессия</Link>
+              <Link to="/info" className="header-button">Инфо</Link>
+            </div>
+            <button
+              onClick={toggleTheme}
+              className="theme-toggle"
+              style={{
+                backgroundColor: isDarkMode ? '#cccccc' : '#fff',
+                color: isDarkMode ? '#fff' : '#fff'
+              }}
+            >
+              <img src={night} alt="night" className="logo-image" />
+            </button>
+            {/* Кнопка для шторки с анимацией */}
+            <div className={`menu-button ${isDrawerOpen ? 'open' : ''}`} onClick={toggleDrawer}>
+              <span className={`line line1 ${isFullyOpened ? 'opened' : ''}`}></span>
+              <span className={`line line2 ${isFullyOpened ? 'opened' : ''}`}></span>
+            </div>
           </div>
-          <button
-            onClick={toggleTheme}
-            className="theme-toggle"
-            style={{
-              backgroundColor: isDarkMode ? '#cccccc' : '#fff',
-              color: isDarkMode ? '#fff' : '#fff'
-            }}
-          >
-            <img src={night} alt="night" className="logo-image" />
-          </button>
-          {/* Кнопка для шторки с анимацией */}
-          <div className={`menu-button ${isDrawerOpen ? 'open' : ''}`} onClick={toggleDrawer}>
-            <span className={`line line1 ${isFullyOpened ? 'opened' : ''}`}></span>
-            <span className={`line line2 ${isFullyOpened ? 'opened' : ''}`}></span>
-          </div>
-        </div>
 
-        {/* Выдвижная шторка */}
-        <div className={`drawer ${isDrawerOpen ? 'open' : ''}`}>
-          <div className="header-buttons">
-            <Link to="/" className="header-button" onClick={toggleDrawer}>Расписание</Link>
-            <Link to="/portfolio" className="header-button" onClick={toggleDrawer}>Портфолио</Link>
-            <Link to="/session" className="header-button" onClick={toggleDrawer}>Сессия</Link>
+          {/* Выдвижная шторка */}
+          <div className={`drawer ${isDrawerOpen ? 'open' : ''}`}>
+            <div className="header-buttons">
+              <Link to="/" className="header-button" onClick={toggleDrawer}>Расписание</Link>
+              <Link to="/portfolio" className="header-button" onClick={toggleDrawer}>Портфолио</Link>
+              <Link to="/session" className="header-button" onClick={toggleDrawer}>Сессия</Link>
+            </div>
           </div>
-        </div>
 
-        {/* Основные маршруты */}
-        <Routes>
-          <Route path="/" element={<ProfileSelection />} />
-          <Route path="/course-selectionte" element={<CourseSelectionTE />} />
-          <Route path="/group-selectionte2" element={<GroupSelectionTE2 />} />
-          <Route path="/TE31B" element={<TE31B />} />
-          <Route path="/group-selectionte3" element={<GroupSelectionTE3 />} />
-          <Route path="/TE21B" element={<TE21B />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-        </Routes>
+          {/* Основные маршруты */}
+          <Routes>
+            <Route path="/" element={<ProfileSelection />} />
+            <Route path="/course-selectionte" element={<CourseSelectionTE />} />
+            <Route path="/group-selectionte2" element={<GroupSelectionTE2 />} />
+            <Route path="/TE31B" element={<TE31B />} />
+            <Route path="/group-selectionte3" element={<GroupSelectionTE3 />} />
+            <Route path="/TE21B" element={<TE21B />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
