ba32f501e1e0ce26d14a0b0abb338f06eb8e896a
JohnyVecher <terra.stepanova@yandex.ru>
2024-11-04 23:41:02 +0500

    daun

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,10 +1,5 @@
-// src/serviceWorkerRegistration.js
-
-
-// Проверяем, поддерживает ли браузер Service Workers
 export function register(config) {
   if ('serviceWorker' in navigator) {
-    // Ожидаем загрузки окна для регистрации SW
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
@@ -41,7 +36,7 @@ export function register(config) {
   }
 }
 
-// Деактивация Service Worker
+
 export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready
