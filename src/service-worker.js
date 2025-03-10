8983528cacfa6f20b4bb6345e89c0e4787896697
JohnyVecher <terra.stepanova@yandex.ru>
2025-03-10 17:37:21 +0500

    работай пж4

--- src/service-worker.js
+++ src/service-worker.js
@@ -8,31 +8,6 @@ import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
 
-// Подключаем Firebase Messaging
-importScripts("https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js");
-
-// Инициализируем Firebase
-firebase.initializeApp({
-  apiKey: "AIzaSyAVj8WCWud9YOQ7yebR7WQOZHhjFmze_-Q",
-  authDomain: "schedulenotifications-a7f31.firebaseapp.com",
-  projectId: "schedulenotifications-a7f31",
-  storageBucket: "schedulenotifications-a7f31.firebasestorage.app",
-  messagingSenderId: "1089118341502",
-  appId: "1:1089118341502:web:2aaced60550327826e8b2f"
-});
-
-const messaging = firebase.messaging();
-
-// Обработчик входящих push-уведомлений в фоне
-messaging.onBackgroundMessage((payload) => {
-  console.log("🔔 Фоновое уведомление получено:", payload);
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-    icon: "/icons/icon-192x192.png",
-  });
-});
-
 // Принудительная активация нового Service Worker сразу после установки
 clientsClaim();
 
