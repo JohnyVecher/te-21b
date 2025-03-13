682db0e58a5a31a63cae5c6b0889c28ade6664ed
JohnyVecher <terra.stepanova@yandex.ru>
2025-03-13 22:15:39 +0500

    Виктор1

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -6,7 +6,7 @@ export function register(config) {
   if ('serviceWorker' in navigator) {
     // Ожидаем загрузки окна для регистрации SW
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = '/service-worker.js';
 
       navigator.serviceWorker
         .register(swUrl)
